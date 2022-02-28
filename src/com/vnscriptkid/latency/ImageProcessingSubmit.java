1129ebbf0fb1b679e67727a067ff6532f667bcb0
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-28 22:23:54 +0700

    feat: image processing submit 1

--- src/com/vnscriptkid/latency/ImageProcessingSubmit.java
+++ src/com/vnscriptkid/latency/ImageProcessingSubmit.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ImageProcessingEx {
+public class ImageProcessingSubmit {
     public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
     public static final String DEST_FILE = "./out/many-flowers.jpg";
 
@@ -18,27 +18,65 @@ public class ImageProcessingEx {
                 BufferedImage.TYPE_INT_RGB);
 
         // todo: time singleThreaded and multiThreaded (try out different numOfThreads)
+        long start = System.currentTimeMillis();
+//        recolorSingleThreaded(originalImage, resultImage);
+        recolorMultiThreaded(originalImage, resultImage, 4);
+        long end = System.currentTimeMillis();
+        long duration = end - start;
+        System.out.println("Duration: " + duration);
 
         File outputFile = new File(DEST_FILE);
         ImageIO.write(resultImage, "jpg", outputFile);
     }
 
-    public static void recolorMultiThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+    public static void recolorMultiThreaded(BufferedImage originalImage, BufferedImage resultImage, int numOfThreads) {
         // todo: takes in numberOfThreads as arg
         // todo: init a list of threads
+        List<Thread> threads = new ArrayList<>();
         // todo: divide whole image into parts horizontally (by height)
         // todo: each part is processed by one thread, need to calculate yStart of each portion
+        int dividedHeight = originalImage.getHeight() / numOfThreads;
+
+        for (int i = 0; i < numOfThreads; i++) {
+            int threadMultiplier = i;
+            threads.add(new Thread(() -> {
+                int xStart = 0;
+                int yStart = threadMultiplier * dividedHeight;
+                recolorImage(originalImage,
+                        resultImage,
+                        xStart,
+                        yStart,
+                        originalImage.getWidth(),
+                        dividedHeight);
+            }));
+        }
         // todo: start each thread
+        for(Thread thread: threads) {
+            thread.start();
+        }
         // todo: hold on the main thread until all threads are done.
+        for(Thread thread: threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
         // todo: recolor whole image in one thread
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
     }
 
     public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int xStart, int yStart,
                                     int width, int height) {
         // todo: process every pixel from top to bottom, left to right, recolor (x,y)
+        for (int x = xStart; x < xStart + width && x < originalImage.getWidth(); x++) {
+            for (int y = yStart; y < yStart + height && y < originalImage.getHeight(); y++) {
+                recolorPixel(originalImage, resultImage, x, y);
+            }
+        }
     }
 
     public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
@@ -99,3 +137,4 @@ public class ImageProcessingEx {
         return rgb & 0x000000FF;
     }
 }
+
