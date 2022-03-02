d5fc1ca7ab3f336318cec2f9e372d1c2d3a932b2
Thanh Nguyen <vnscriptkid@gmail.com>
2022-03-02 20:28:50 +0700

    feat: metrics ex

---
+++ src/com/vnscriptkid/concurrencychallenges/MetricsFinal.java
@@ -0,0 +1,78 @@
+package com.vnscriptkid.concurrencychallenges;
+
+import java.util.Random;
+
+public class MetricsFinal {
+    public static void main(String[] args) {
+        Metrics metrics = new Metrics();
+        BusinessLogic businessLogic1 = new BusinessLogic(metrics);
+        BusinessLogic businessLogic2 = new BusinessLogic(metrics);
+        MetricsPrinter metricsPrinter = new MetricsPrinter(metrics);
+
+        businessLogic1.start();
+        businessLogic2.start();
+        metricsPrinter.start();
+    }
+
+    public static class MetricsPrinter extends Thread {
+        Metrics metrics;
+
+        public MetricsPrinter(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Average now: " + metrics.getAverage());
+            }
+        }
+    }
+
+    public static class BusinessLogic extends Thread {
+        Metrics metrics;
+
+        public BusinessLogic(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long start = System.currentTimeMillis();
+
+                try {
+                    Thread.sleep(new Random().nextInt(10));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                long end = System.currentTimeMillis();
+
+                this.metrics.addSample(end - start);
+            }
+        }
+    }
+
+    public static class Metrics {
+        long count = 0;
+        volatile double average = 0.0;
+
+        public synchronized void addSample(long sample) {
+            double currentSum = count * average;
+
+            count++;
+
+            average = (currentSum + sample) / count;
+        }
+
+        public double getAverage() {
+            return average;
+        }
+    }
+}
---
+++ src/com/vnscriptkid/concurrencychallenges/readme.md
@@ -0,0 +1,7 @@
+## sync
+- why not putting `synchronized` to all methods of class?
+- ways to make a section critical section?
+- atomic ops:
+  - ref assignments
+  - assignments to primitive types except long, double (int, short, byte, float, char, boolean)
+  - assignments to long, double (with `volatile`)
